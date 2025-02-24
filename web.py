b94d5e78dffde027590feec58c1b1e65dc2063a7
glaudistonneto <glaudistonson@gmail.com>
2025-02-24 18:03:34 -0300

    fixed 'loop error' and 'session state cleanup'

--- web.py
+++ web.py
@@ -14,12 +14,12 @@ st.subheader("This is my too App.")
 st.write("This app is to increase your productivity.")
 
 
-for index, todo in todos:
+for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
-        del st.session_state[todo]
+        del st.session_state[f"todo_{index}"]
         st.rerun()
 
 st.text_input(label="", placeholder="Add new todo... ", on_change=add_todo, key='new_todo')
