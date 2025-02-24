984ae1458a462cb58b38afb2e1d506802214a605
glaudistonneto <glaudistonson@gmail.com>
2025-02-24 17:58:36 -0300

    web app finished

--- web.py
+++ web.py
@@ -11,12 +11,15 @@ todos = functions.get_todos()
 
 st.title("My Todo App")
 st.subheader("This is my too App.")
-st.write("This app is to increase your prouctivity.")
+st.write("This app is to increase your productivity.")
 
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in todos:
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="", placeholder="Add new todo... ", on_change=add_todo, key='new_todo')
-
-st.session_state
